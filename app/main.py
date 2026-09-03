26fe98542c8f0b5a330a60fa407f5dda26d94609
Samuel Tanahuvia <isra025thvia@gmail.com>
2026-09-02 20:05:07 -0600

    Prueba de fallo en pipeline

--- app/main.py
+++ app/main.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"mensaje": "Sistema de Inventario Activo", "estado": "Operativo"}
+    return {"mensaje": "Sistema Actualizado", "estado": "Operativo"}
